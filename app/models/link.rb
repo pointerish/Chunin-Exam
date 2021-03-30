54512c3e8b3c68f14f4075917e804343a08eb84d
Josias Alvarado <josiasjag@gmail.com>
2021-03-30 11:55:09 -0600

    Add hashids and update validator

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,14 @@
+require 'httparty'
+
+class ValidUrlValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    if value.present?
+      HTTParty.get(value).ok?
+    end
+  end
+end
+
 class Link < ApplicationRecord
+  include Hashid::Rails
   validates :url, presence: true, valid_url: true
 end
--- app/validators/url_validator.rb
+++
@@ -1,9 +0,0 @@
-require 'httparty'
-
-class ValidURLValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    if value.present?
-      HTTParty.get(value).ok?
-    end
-  end
-end
